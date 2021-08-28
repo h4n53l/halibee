a90266c459f3b131853a6d24a53f936a95a1b08e
= Jonathan Hansel Awo <= jonhan17@gmail.com>
2021-08-28 01:31:12 +0100

    ll

--- halibee/components/layout/index.tsx
+++ halibee/components/layout/index.tsx
@@ -6,12 +6,7 @@ export default function Layout({ children }) {
             <Navbar />
             {children}
             <footer className="flex items-center justify-center w-full h-24 border-t">
-                <a
-                    className="flex items-center justify-center"
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
+                <a>
                     By HaLi
                 </a>
             </footer>
