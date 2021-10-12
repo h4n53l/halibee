0cb60d336a2d598b2fa0dc789d19ea56bb35ef95
= Jonathan Hansel Awo <= jonhan17@gmail.com>
2021-10-12 22:03:53 +0100

    attempted adding cometchat

--- halibee/components/themeToggle.tsx
+++ halibee/components/themeToggle.tsx
@@ -22,9 +22,10 @@ export default function ThemeToggle() {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
             ></path>
           </svg>
+          
         </button>
       ) : (
         <button
@@ -42,7 +43,7 @@ export default function ThemeToggle() {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
             ></path>
           </svg>
         </button>
--- halibee/pages/messages/[messages].tsx
+++ halibee/pages/messages/[messages].tsx
@@ -1,3 +1,7 @@
+import dynamic from "next/dynamic";
+import { CometChat } from "@cometchat-pro/chat";
+
+
 export default function() {
     return (
         <div>
