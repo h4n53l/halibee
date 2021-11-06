cdfb69859c72f3db310293ef17d69a0cdd99f304
= Jonathan Hansel Awo <= jonhan17@gmail.com>
2021-11-06 17:28:26 +0000

    removed middleware.

--- halibee/pages/_middleware.ts
+++
@@ -1,6 +0,0 @@
-import type { NextFetchEvent, NextRequest } from 'next/server'
-import { auth } from '../modules/firebase/initialiseFirebase'
-
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  return auth.currentUser
-}
